866ff03cf52430caeb72ad80b2ba53b9a1504067
Haito <hajtosek@gmail.com>
2017-07-31 15:01:12 +0200

    added dns support

--- lib/mnesia_test.ex
+++ lib/mnesia_test.ex
@@ -2,7 +2,7 @@ defmodule MnesiaTest do
   use Application
 
   def start(_,_) do
-    :inet_res.lookup('localhost',:in,:a)
+    :inet_res.lookup(System.get_env("APP_NAME"),:in,:a)
     |> Enum.each(fn {a,b,c,d} ->
       IO.inspect Node.connect( :"#{System.get_env("MY_POD_NAMESPACE")}@#{a}.#{b}.#{c}.#{d}" ) #<- connect
     end)
