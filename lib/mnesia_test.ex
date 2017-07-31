70951dd190e94ed16ea4b3b52988ff297aa9130e
Haito <hajtosek@gmail.com>
2017-07-31 15:47:45 +0200

    Timer experiment

--- lib/mnesia_test.ex
+++ lib/mnesia_test.ex
@@ -2,6 +2,7 @@ defmodule MnesiaTest do
   use Application
 
   def start(_,_) do
+    :timer.sleep(5000);
     :inet_res.lookup(System.get_env("APP_NAME")<>".default.svc.cluster.local" |> IO.inspect,:in,:a)
     |> IO.inspect
     |> Enum.each(fn {a,b,c,d} ->
