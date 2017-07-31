7d0d2cf6d21860c47e8d94483f89a51dc459bdac
Haito <hajtosek@gmail.com>
2017-07-31 15:10:13 +0200

    update

--- lib/mnesia_test.ex
+++ lib/mnesia_test.ex
@@ -2,7 +2,8 @@ defmodule MnesiaTest do
   use Application
 
   def start(_,_) do
-    :inet_res.lookup(System.get_env("APP_NAME"),:in,:a)
+    :inet_res.lookup(System.get_env("APP_NAME")<>".default.svc.cluster.local",:in,:a)
+    |> IO.inspect
     |> Enum.each(fn {a,b,c,d} ->
       IO.inspect Node.connect( :"#{System.get_env("MY_POD_NAMESPACE")}@#{a}.#{b}.#{c}.#{d}" ) #<- connect
     end)
