3aa22e746328d3321be68c7d1c01a983d37c7f04
Haito <hajtosek@gmail.com>
2017-07-31 16:32:13 +0200

    Created extractor

--- lib/mnesia_test.ex
+++ lib/mnesia_test.ex
@@ -2,12 +2,23 @@ defmodule MnesiaTest do
   use Application
 
   def start(_,_) do
-    :timer.sleep(5000);
-    :inet_res.nslookup(System.get_env("APP_NAME")<>".default.svc.cluster.local." |> IO.inspect |> to_charlist,:in,:a)
-    |> IO.inspect
-    |> Enum.each(fn {a,b,c,d} ->
-      IO.inspect Node.connect( :"#{System.get_env("MY_POD_NAMESPACE")}@#{a}.#{b}.#{c}.#{d}" ) #<- connect
-    end)
+    try do
+      :timer.sleep(5000);
+      :inet_res.nslookup(System.get_env("APP_NAME")<>".default.svc.cluster.local." |> IO.inspect |> to_charlist,:in,:a)
+      |> IO.inspect
+      |> extract_ip_list()
+      |> IO.inspect
+      |> Enum.each(fn {a,b,c,d} ->
+        IO.inspect Node.connect( :"#{System.get_env("MY_POD_NAMESPACE")}@#{a}.#{b}.#{c}.#{d}" ) #<- connect
+      end)
+    rescue
+      err ->err
+    end
+
     Task.Supervisor.start_link() #Just to stahp the sys
+  end
+
+  def extract_ip_list({:ok,{:dns_rec, _head, _query, list_rr, _, _}}) do
+    Enum.map(list_rr, fn {:dns_rr, _link, _class, _type , _ ,_ , ip ,_undefined , _array ,_bool} -> ip end)
   end
 end
--- test/mnesia_test_test.exs
+++ test/mnesia_test_test.exs
@@ -2,7 +2,14 @@ defmodule MnesiaTestTest do
   use ExUnit.Case
   doctest MnesiaTest
 
+  @sample_DNS {:ok,
+               {:dns_rec,
+                {:dns_header, 1, true, :query, false, false, true, true, false, 0},
+                [{:dns_query, 'erlangpl-demo-mnesia.default.svc.cluster.local', :a, :in}],
+                [{:dns_rr, 'erlangpl-demo-mnesia.default.svc.cluster.local', :a, :in, 0, 24, {172, 17, 0, 3}, :undefined, [], false}], [], []}
+  }
+
   test "the truth" do
-    assert 1 + 1 == 2
+    MnesiaTest.extract_ip_list(@sample_DNS) == [{172, 17, 0, 3}]
   end
 end
